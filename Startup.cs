c60724664d8c948d63234680d18ca796e04ed500
Matheus Rondon Rudolf <matheusrudolf@hotmail.com>
2019-07-30 21:06:33 -0400

    Alteração projeto

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace ProBoostrapFramework
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,8 @@ namespace ProBoostrapFramework
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseMvc(rotas => {
+                rotas.MapRoute(name:"default", template:"{controller=Home}/{action=Index}");
             });
         }
     }
